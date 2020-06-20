f3aa97cc57e2c699f2e330cfcc41abce3d827265
lokentran <thanhtungmarine@gmail.com>
2020-06-20 22:47:24 +0700

    Dislay time of server

---
+++ server_time/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Time is: <?php echo date('y-m-d h:m:s'); ?></h1>
+
+    
+</body>
+</html>
